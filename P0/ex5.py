4b40927432cae23b4d848d77ce67793af25d20e5
lucia <l.berzosa.2021@alumnos.urjc.es>
2022-02-16 18:42:35 +0100

    creation of calculate_weight

--- P0/ex5.py
+++ P0/ex5.py
@@ -1,10 +1,8 @@
 import seq0
 list_genes = ["U5.txt", "FRAT1.txt", "ADA.txt", "FXN.txt", "RNU6_269P.txt"]
 FOLDER = "../Session04/"
-
 for f in list_genes:
     filename = FOLDER + f
     seq = seq0.seq_read_fasta(filename)
-    count_A, count_C, count_G, count_T = seq0.count_bases_seq(seq)
-    print("GEN", f)
-    print("A:", count_A, "\nC:", count_C, "\nG:", count_G, "\nT:", count_T)
+    d = seq0.count_bases(seq)
+    print("gen", f, "-->", d)
--- P0/seq0.py
+++ P0/seq0.py
@@ -17,6 +17,22 @@ def seq_read_fasta(filename):
     seq = seq[seq.find("\n"):].replace("\n", "")
     return seq
 
+def count_bases_seq(seq):
+    count_A = 0
+    count_C = 0
+    count_G = 0
+    count_T = 0
+    for i in seq:
+        if i == "A":
+            count_A += 1
+        elif i == "C":
+            count_C += 1
+        elif i == "G":
+            count_G += 1
+        elif i == "T":
+            count_T += 1
+    return count_A, count_C, count_G, count_T
+
 def count_bases(seq):
     d = {"A": 0, "C": 0, "G": 0, "T": 0}
     for b in seq:
